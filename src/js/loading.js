e474813054220f63d038edb7b33d1ba21f98b676
정승일 <jungsi1217@naver.com>
2023-06-17 22:02:20 +0900

    로딩 메세지 추가

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,12 +1,17 @@
 function Loading (printAnswer){
     $form.innerHTML = '';
     if(!printAnswer){
+        let div = document.createElement("div")
         let img = document.createElement("img")
+        let p = document.createElement("p")
 
-        img.classList.add("load")
-
+        div.classList.add("load")
         img.setAttribute("src","./src/img/Book_loading.png")
-        
-        $form.appendChild(img)
+        p.classList.add("loadingM")
+        p.innerText = '10 ~ 20 초 정도의 시간이 걸리는 점 양해 부탁드립니다.'
+
+        div.appendChild(p)
+        div.appendChild(img)
+        $form.appendChild(div)
     }
 }
